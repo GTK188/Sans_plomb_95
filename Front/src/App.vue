38124953b125de015be38b0a388d3d1a109918a7
Josse DE OLIVEIRA <contactme@webjosse.fr>
2022-12-02 02:17:16 +0100

    J'enleve vue router car ça sert à r

--- Front/src/App.vue
+++ Front/src/App.vue
@@ -1,16 +1,36 @@
+<script setup>
+import { ref, watch } from "vue";
+import Game from "@/components/Game.vue";
+let url = ref(window.location.pathname);
+watch(url, (path) => {
+  window.history.pushState("", "", url.value);
+});
+
+/**
+ *
+ * @param {Event} event
+ */
+function onHeaderClick(event) {
+  event.preventDefault();
+  if (!event.target.href) return;
+  url.value = event.target.getAttribute('href');
+}
+</script>
+
 <template>
   <div>
     <header>
-      <a href="">
-        <img src="/images/logo.png" alt="Logo MSTere" id="hlogo"/>
+      <a href="" @click="onHeaderClick" class="logolink">
+        <img id="hlogo" src="/images/logo.png" alt="Logo MSTere" />
       </a>
-      <a href="/play">Jouer</a>
-      <a href="/infos">Informations</a>
+      <a href="/play" @click="onHeaderClick">Jouer</a>
+      <a href="/infos" @click="onHeaderClick">Informations</a>
     </header>
 
-    <div>
-      <router-view></router-view>
-    </div>
+    <main v-if="url == '/'"></main>
+    <main v-else-if="url.startsWith('/play')"><Game /></main>
+    <main v-else-if="url == '/infos'"></main>
+    <main v-else></main>
   </div>
 </template>
 
@@ -37,5 +57,9 @@ header a:hover {
 }
 #hlogo{
   height: 2em;
+}
+
+header a.logolink{
+  padding: 0;
 }
 </style>
